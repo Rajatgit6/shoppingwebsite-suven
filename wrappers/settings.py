bf87eb45a0cc0f6093ddcebb5bb5ff5d6bfb50eb
Simanchal Pattanayak <83402452+SPdeveloper-19@users.noreply.github.com>
2021-08-15 01:25:29 +0530

    Update settings.py

--- wrappers/settings.py
+++ wrappers/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
